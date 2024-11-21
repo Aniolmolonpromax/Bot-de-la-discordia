fa52c1f6577c77bcf160ea7eacfc0be3abf5783b
Aniolmolonpromax <aniol.decruz@olot.epiaedu.cat>
2024-11-21 20:19:55 +0100

    Update 1 bot.py

--- bot.py
+++ bot.py
@@ -63,33 +63,19 @@ async def userinfo_error(ctx: commands.Context, error: commands.CommandError):
     else:
         traceback.print_exception(type(error), error, error.__traceback__)
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
+@bot.command()
+async def guess(ctx):
+    await ctx.send('Guess a number between 1 and 10.')
+    def is_correct(m):
+        return m.author == ctx.author and m.content.isdigit()
+    answer = random.randint(1, 10)
+    try:
+        guess = await bot.wait_for('message', check=is_correct, timeout=5.0)
+    except asyncio.TimeoutError:
+        return await ctx.send(f'Sorry, you took too long! It was {answer}.')
+    if int(guess.content) == answer:
+        await ctx.send('You are right!')
+    else:
+        await ctx.send(f'Oops. It was actually {answer}.')
 
-    async def on_message(self, message):
-        if message.author.id == self.user.id:
-            return
-
-        if message.content.startswith('$guess'):
-            await message.channel.send('Guess a number between 1 and 10.')
-
-            def is_correct(m):
-                return m.author == message.author and m.content.isdigit()
-
-            answer = random.randint(1, 10)
-
-            try:
-                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
-            except asyncio.TimeoutError:
-                return await message.channel.send(f'Sorry, you took too long it was {answer}.')
-
-            if int(guess.content) == answer:
-                await message.channel.send('You are right!')
-            else:
-                await message.channel.send(f'Oops. It is actually {answer}.')
-
-client = MyClient(intents=intents)
-client.run(token)
 bot.run(token)
